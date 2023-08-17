47a36bcec4b63d03b952f6eea0fc85494d612d1d
yonathanbaez25 <yacevedo170@gmail.com>
2023-08-17 00:46:56 -0400

    Deploy 13

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-import axios from "axios";
+//import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:3001";
-axios.defaults.baseURL = "back-rm-production.up.railway.app";
+//axios.defaults.baseURL = "back-rm-production.up.railway.app";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
